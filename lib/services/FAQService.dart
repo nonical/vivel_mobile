01a5af887f62839aab6f87f92e4b344264134960
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-18 01:08:01 +0100

    add query string

--- lib/services/FAQService.dart
+++ lib/services/FAQService.dart
@@ -4,7 +4,7 @@ import 'APIService.dart';
 
 class FAQService {
   static Future<List<FAQ>> Get() async {
-    var faqs = await APIService.Get("faq", null);
+    var faqs = await APIService.Get("faq", {'answered': 'true'});
     if (faqs != null) {
       return faqs.map((x) => FAQ.fromJson(x)).toList();
     }
