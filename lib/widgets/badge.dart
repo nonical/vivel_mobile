7601415f3c6b92bc8bdee1f79d8b9dfe3f887851
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-19 15:59:59 +0100

    Add CardContainer widget

---
+++ lib/widgets/badge.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:vivel_mobile/widgets/card.dart';
+
+class BadgeWidget extends StatelessWidget {
+  const BadgeWidget({Key? key, required this.image, required this.text})
+      : super(key: key);
+
+  final ImageProvider<Object> image;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(bottom: 40),
+        child: CardContainer(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Center(
+                      child: Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: image)),
+              ))),
+              Expanded(child: Center(child: Text(text)))
+            ],
+          ),
+        ));
+  }
+}
