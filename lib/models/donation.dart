267341d10954e66d37bad1d0a87d7b22f1a6b62e
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-19 17:01:03 +0100

    add my donations page (#8)
    * add my donations page
    
    * rename donation widget to donation list item widget

---
+++ lib/models/donation.dart
@@ -0,0 +1,37 @@
+class Donation {
+  final String donationId;
+  final String driveId;
+  final String scheduledAt;
+  final int amount;
+  final String status;
+  final String note;
+  final num leukocyteCount;
+  final num erythrocyteCount;
+  final num plateletCount;
+
+  Donation({
+    required this.donationId,
+    required this.driveId,
+    required this.scheduledAt,
+    required this.amount,
+    required this.status,
+    required this.note,
+    required this.leukocyteCount,
+    required this.erythrocyteCount,
+    required this.plateletCount,
+  });
+
+  factory Donation.fromJson(Map<String, dynamic> json) {
+    return Donation(
+      donationId: json["donationId"],
+      driveId: json["driveId"],
+      scheduledAt: json["scheduledAt"],
+      amount: json["amount"] ?? 0,
+      status: json["status"],
+      note: json["note"] ?? "",
+      leukocyteCount: json["leukocyteCount"] ?? 0,
+      erythrocyteCount: json["erythrocyteCount"] ?? 0,
+      plateletCount: json["plateletCount"] ?? 0,
+    );
+  }
+}
---
+++ lib/widgets/donation_list_item.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:vivel_mobile/models/donation.dart';
+
+class DonationListItemWidget extends StatelessWidget {
+  final Donation donation;
+
+  const DonationListItemWidget({Key? key, required this.donation})
+      : super(key: key);
+
+  Widget statusText(String status) {
+    Color textColor = Color.fromRGBO(153, 153, 153, 100);
+
+    switch (status) {
+      case "Approved":
+        textColor = const Color.fromRGBO(0, 196, 140, 100);
+        break;
+      case "Rejected":
+        textColor = const Color.fromRGBO(255, 100, 124, 100);
+        break;
+      case "Scheduled":
+        textColor = const Color.fromRGBO(242, 153, 74, 100);
+        break;
+    }
+    return (Text(status, style: TextStyle(color: textColor)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => {}, // TODO: add link to donation page
+      child: Container(
+        color: Colors.white,
+        height: 130,
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(255, 100, 124, 100),
+                          shape: BoxShape.circle),
+                      child: Center(child: Image.asset('assets/activity.png')),
+                    )
+                  ],
+                )),
+            Expanded(
+              flex: 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 10, right: 10),
+                          child: Text("DZ Hospital",
+                              style: const TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.w500)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: statusText(donation.status),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/chevron-right.png'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
