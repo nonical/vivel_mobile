8bbe4293af7c496e26dbae6b7d0d3e20736aad50
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-20 02:45:10 +0100

    add style to profile navigation

--- lib/widgets/NavigationBar/profile_navigation.dart
+++ lib/widgets/NavigationBar/profile_navigation.dart
@@ -5,6 +5,12 @@ class ProfileNavigation extends AppBar {
       : super(
           title: Text("Profile"),
           centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          titleTextStyle: const TextStyle(color: Colors.black, fontSize: 17),
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
           actions: [
             IconButton(
                 onPressed: () => {},
