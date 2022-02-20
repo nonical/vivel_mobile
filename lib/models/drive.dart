82bc2ab5ae575bb8d8ea854639c77ee3a7f0d5c6
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-20 17:56:34 +0100

    Add missing field to Drive model

--- lib/models/drive.dart
+++ lib/models/drive.dart
@@ -1,30 +1,35 @@
 class Drive {
   final String driveId;
+  final String hospitalId;
   final String date;
   final String bloodType;
   final num amount;
   final String status;
   final bool urgency;
   final String createdAt;
+  final String updatedAt;
 
   Drive(
       {required this.driveId,
+      required this.hospitalId,
       required this.date,
       required this.bloodType,
       required this.amount,
       required this.status,
       required this.urgency,
-      required this.createdAt});
+      required this.createdAt,
+      required this.updatedAt});
 
   factory Drive.fromJson(Map<String, dynamic> json) {
     return Drive(
-      driveId: json["driveId"],
-      date: json["date"],
-      bloodType: json["bloodType"],
-      amount: json["amount"],
-      status: json["status"],
-      urgency: json["urgency"],
-      createdAt: json["createdAt"],
-    );
+        driveId: json["driveId"],
+        hospitalId: json["hospitalId"],
+        date: json["date"],
+        bloodType: json["bloodType"],
+        amount: json["amount"],
+        status: json["status"],
+        urgency: json["urgency"],
+        createdAt: json["createdAt"],
+        updatedAt: json["updatedAt"]);
   }
 }
