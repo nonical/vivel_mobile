98087ec03b6f4b905e218c8b529037b55633494d
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-21 00:49:28 +0100

    Add backgroundColor param to BackNavigation

--- lib/widgets/navigation_bar/back_navigation.dart
+++ lib/widgets/navigation_bar/back_navigation.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:vivel_mobile/constants/colors.dart';
 
 class BackNavigation extends AppBar {
-  BackNavigation(String title, {Key? key})
+  BackNavigation(String title, {Key? key, Color backgroundColor = Colors.white})
       : super(
           key: key,
           title: Text(title),
           elevation: 0,
-          backgroundColor: Colors.white,
+          backgroundColor: backgroundColor,
           titleTextStyle: const TextStyle(color: BLACK, fontSize: 17),
           iconTheme: const IconThemeData(
             color: BLACK,
