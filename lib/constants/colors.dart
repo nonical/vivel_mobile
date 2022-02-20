0a2b82cd158f6e33eb88986409c0574adda3685e
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-20 03:36:13 +0100

    Add text styles and colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/widgets.dart';
+
+const BLACK = Color.fromRGBO(21, 21, 34, 1);
+
+const RED = Color.fromRGBO(255, 100, 124, 1);
+const RED1 = Color.fromRGBO(255, 152, 168, 1);
+
+const GRAY3 = Color.fromRGBO(153, 153, 153, 1);
+const GRAY4 = Color.fromRGBO(228, 228, 228, 1);
---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/widgets.dart';
+
+const BIG_HEADER = TextStyle(fontWeight: FontWeight.w600, fontSize: 48);
+
+const HEADING3 = TextStyle(fontWeight: FontWeight.w600, fontSize: 22);
+const HEADING4 = TextStyle(fontWeight: FontWeight.w300, fontSize: 20);
+const HEADING6 = TextStyle(fontWeight: FontWeight.w600, fontSize: 15);
+
+const CAPTION1 = TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
