3575a86bc39f8e353137f6df1290292d42304c87
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-20 19:17:33 +0100

    Add Donation page to HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vivel_mobile/pages/active_drives_page.dart';
 import 'package:vivel_mobile/pages/badges_page.dart';
+import 'package:vivel_mobile/pages/donation_page.dart';
 import 'package:vivel_mobile/pages/my_donations_page.dart';
 import 'package:vivel_mobile/pages/user_notifications_page.dart';
 import 'package:vivel_mobile/widgets/NavigationBar/home_navigation.dart';
@@ -64,6 +65,17 @@ class _HomePageState extends State<HomePage> {
                             builder: (context) => const MyDonationsPage(
                                   userId:
                                       '49649a15-21a8-4048-abb5-2b17f5cb4efa',
+                                )))
+                  }),
+          TextButton(
+              child: Text("Go to Donation"),
+              onPressed: () => {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const DonationPage(
+                                  donationId:
+                                      '6ef5f7c2-821f-4b01-b661-47be01c6f714',
                                 )))
                   })
         ]));
