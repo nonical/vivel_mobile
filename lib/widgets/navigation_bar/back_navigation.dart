5b45295fba12abd84558d92e0f2c98f1c6d541d7
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-21 23:16:59 +0100

    Extend BackNavigation with backgroundColor and systemOverlayStyle

--- lib/widgets/navigation_bar/back_navigation.dart
+++ lib/widgets/navigation_bar/back_navigation.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vivel_mobile/constants/colors.dart';
 
 class BackNavigation extends AppBar {
-  BackNavigation(String title, {Key? key, Color backgroundColor = Colors.white})
+  BackNavigation(String title,
+      {Key? key,
+      Color backgroundColor = Colors.white,
+      SystemUiOverlayStyle? systemOverlayStyle})
       : super(
-          key: key,
-          title: Text(title),
-          elevation: 0,
-          backgroundColor: backgroundColor,
-          titleTextStyle: const TextStyle(color: BLACK, fontSize: 17),
-          iconTheme: const IconThemeData(
-            color: BLACK,
-          ),
-          centerTitle: true,
-        );
+            key: key,
+            title: Text(title),
+            elevation: 0,
+            backgroundColor: backgroundColor,
+            titleTextStyle: const TextStyle(color: BLACK, fontSize: 17),
+            iconTheme: const IconThemeData(
+              color: BLACK,
+            ),
+            centerTitle: true,
+            systemOverlayStyle: systemOverlayStyle);
 }
