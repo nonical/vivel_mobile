43d10dde36b552461b24f469cd03b9dcda66ce25
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-27 03:22:48 +0100

    Merge to previous

--- lib/utils/oauth_config.dart
+++ lib/utils/oauth_config.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const clientId = 'vivel.mobile';
 const clientSecret = '2bc63e15-a44a-42e9-8597-2fcdee8350e0';
 const redirectUrl = 'com.nonical.vivel:/callback';
-const scopes = ['openid', 'profile', 'scope1'];
+const scopes = ['openid', 'profile', 'scope1', 'offline_access'];
 
 final issuer = dotenv.env['IDENTITY_URL']!;
 final discoveryUrl = '$issuer/.well-known/openid-configuration';
@@ -18,8 +20,26 @@ final serviceConfiguration = AuthorizationServiceConfiguration(
     tokenEndpoint: tokenEndpoint,
     endSessionEndpoint: endSessionEndpoint);
 
-final oauthConfig = AuthorizationTokenRequest(clientId, redirectUrl,
-    clientSecret: clientSecret,
-    serviceConfiguration: serviceConfiguration,
-    scopes: scopes,
-    allowInsecureConnections: true);
+AuthorizationTokenRequest getAuthorizationTokenRequest() {
+  return AuthorizationTokenRequest(clientId, redirectUrl,
+      clientSecret: clientSecret,
+      issuer: issuer,
+      scopes: scopes,
+      allowInsecureConnections: true);
+}
+
+TokenRequest getTokenRequest(String? refreshToken) {
+  return TokenRequest(clientId, redirectUrl,
+      clientSecret: clientSecret,
+      refreshToken: refreshToken,
+      issuer: issuer,
+      allowInsecureConnections: true);
+}
+
+Map<String, dynamic> parseIdToken(String idToken) {
+  final parts = idToken.split(r'.');
+  assert(parts.length == 3);
+
+  return jsonDecode(
+      utf8.decode(base64Url.decode(base64Url.normalize(parts[1]))));
+}
